8554cb5f97c049c6099fa64bae79345d8562afbf
Angelique Nehmzow <angelique@nehmzow.com>
2019-11-01 12:43:36 -0700

    schema file, add comment generator

--- db/data_generation.js
+++ db/data_generation.js
@@ -3,20 +3,20 @@ const csv = require('csv-parse');
 const faker = require('faker');
 const path = require('path');
 
-const ARTIST_MAX_COUNT = ALBUM_MAX_COUNT = USER_MAX_COUNT = 20;
+const SONG_MAX_COUNT = 20;
+const ARTIST_MAX_COUNT = ALBUM_MAX_COUNT = USER_MAX_COUNT = 5;
 const exampleDataFilepath = path.resolve('db', 'data', 'example_song_data.csv');
 
 function generateArtistData() {
   var artists = []
   for (let i = 1; i <= ARTIST_MAX_COUNT; i++) {
-    var album = [];
     var record = {
       // artistId: i,
       artistName: faker.lorem.words(faker.random.number({min: 1, max: 5}))
     }
     artists.push(record);
   }
-  // save to file;
+  // write to file
   return artists;
 }
 
@@ -26,16 +26,16 @@ function generateAlbumData() {
     var record = {
       // albumId: i,
       albumName: faker.lorem.words(faker.random.number({min: 1, max: 7})),
-      // albumReleaseDate
-      // albumArtUrl
+      albumReleaseDate: faker.date.past(3), // random date in the past 3 years
+      albumArtUrl: 'https://i1.sndcdn.com/artworks-d9701706-6d90-4dab-8639-33d432e2dab7-0-t500x500.jpg'
     }
     albums.push(record);
   }
-  // save to file;
+  // write to file
   return albums;
 }
 
-function generateSongData(primaryRecordsCount) {
+function generateSongData() {
   var exampleSongs = [];
   var songs = [];
   fs.createReadStream(exampleDataFilepath, {encoding: 'utf-8'})
@@ -43,7 +43,7 @@ function generateSongData(primaryRecordsCount) {
     .on('data', (song) => exampleSongs.push(song))
     .on('end', () => {
       var exampleSongIndex = 0;
-      for (let i = 1; i <= primaryRecordsCount; i++) {
+      for (let i = 1; i <= SONG_MAX_COUNT; i++) {
         exampleSongIndex = (exampleSongIndex === exampleSongs.length - 1) ? 0 : exampleSongIndex + 1; // rotate through example songs
         var exampleSong = exampleSongs[exampleSongIndex];
         var tags = ['# Electronic', '# Rock', '# Alternative', '# Rap', '# Classical', '# Country', '# Jazz', '# Pop', '# Punk'];
@@ -57,14 +57,14 @@ function generateSongData(primaryRecordsCount) {
           songArtColorLight: exampleSong[4],
           songArtColorDark: exampleSong[5],
           songDuration: Number(exampleSong[3]),
-          songWaveForm: exampleSong[6],
+          songWaveform: exampleSong[6],
           tag: tags[faker.random.number(tags.length - 1)], // random tag
-          datePosted: faker.date.past(3) // random date in the past 3 years
+          datePosted: faker.date.past() // random date in the past year
         }
         songs.push(record);
       }
       // write to file
-      return songs;
+      console.log(songs);
     });
 }
 
@@ -77,12 +77,29 @@ function generateUserData() {
     }
     users.push(record);
   }
+  // write to file
   return users;
 }
 
-// function generateCommentData() {
-  //
-// }
+function generateCommentData() {
+  var comments = [];
+  for (let i = 1; i <= SONG_MAX_COUNT; i++) {
+    var songCount = faker.random.number(5) // generate 0 - 5 comments per song
+    for (let j = 0; j < songCount; j++) {
+      var record = {
+        // commentId: i + j,
+        songId: i,
+        userId: faker.random.number({min: 1, max: USER_MAX_COUNT}),
+        commentId: faker.lorem.words(faker.random.number({min: 1, max: 10})),
+        secondInSong: faker.random.number(135), // random second less than min example song duration
+        datePosted: faker.date.past() // random date in the past year
+      }
+      comments.push(record);
+    }
+  }
+  // write to file
+  return comments;
+}
 
 exports.generateSongData = generateSongData;
 exports.generateArtistData = generateArtistData;
--- db/schema.sql
+++ db/schema.sql
@@ -1,45 +1,9 @@
-DROP DATABASE IF EXISTS 5cloud_song_display;
+DROP DATABASE IF EXISTS aircloudy;
+CREATE DATABASE aircloudy;
+\c aircloudy;
 
-CREATE DATABASE 5cloud_song_display;
-
-USE 5cloud_song_display;
-
+CREATE TABLE artists
+CREATE TABLE albums
 CREATE TABLE songs
-(
-    Id INT
-        NOT NULL
-    AUTO_INCREMENT KEY,
-    song_id VARCHAR
-    (255) UNIQUE,
-    song_name VARCHAR
-    (255),
-    artist_name VARCHAR
-    (255),
-    upload_time BIGINT,
-    tag VARCHAR
-    (255),
-    song_art_url VARCHAR
-    (255),
-    song_data_url VARCHAR
-    (255),
-    background_light VARCHAR
-    (255),
-    background_dark VARCHAR
-    (255),
-    waveform_data JSON,
-    song_duration INT
-);
-
-    CREATE TABLE comments
-    (
-        Id INT
-            NOT NULL
-        AUTO_INCREMENT KEY,
-    song_id VARCHAR
-        (20),
-    user_name VARCHAR
-        (255),
-    time_stamp INT,
-    comment VARCHAR
-        (255)
-);
+CREATE TABLE users
+CREATE TABLE song_comments
