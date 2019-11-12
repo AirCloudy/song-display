a5316c304f7074c34950e54e8ea3aadb1d0899ff
Angelique Nehmzow <angelique@nehmzow.com>
2019-11-11 18:09:47 -0800

    add rps max to k6 files

--- http_get.js
+++ http_get.js
@@ -3,7 +3,8 @@ const SONG_MAX_COUNT = 10000000; // 10 million
 
 export let options = {
   vus: 200,
-  duration: "1m"
+  rps: 1200,
+  duration: "5m"
 };
 
 function getRndBias(min, max, bias, influence) {
@@ -13,34 +14,7 @@ function getRndBias(min, max, bias, influence) {
 }
 
 export default function() {
-  var songId = getRndBias(1, SONG_MAX_COUNT, SONG_MAX_COUNT * 0.8, 0.95);
+  // var songId = getRndBias(1, SONG_MAX_COUNT, SONG_MAX_COUNT * 0.8, 0.95);
+  var songId = Math.floor(Math.random() * (SONG_MAX_COUNT - (9999000) + 1)) + 9999000;
   http.get(`http://localhost:5001/songs/${songId}`);
 };
-
-// import { sleep } from "k6";
-// import http from "k6/http";
-// const SONG_MAX_COUNT = 10000000; // 10 million
-
-// var desiredRPS = 950;
-// var RPSperVU = 4;
-// var VUsRequired = Math.round(desiredRPS / RPSperVU);
-
-// export let options = {
-//   vus: VUsRequired,
-//   duration: '15s',
-// };
-
-// export default function() {
-//   var iterationStart = new Date().getTime(); // timestamp in ms
-//   var songId = Math.floor(Math.random() * SONG_MAX_COUNT) + 1; // pick random song
-
-//   for (let i of Array(RPSperVU).keys()) {
-//     http.get(`http://localhost:5001/songs/${songId}`);
-//   }
-
-//   var iterationDuration = (new Date().getTime() - iterationStart) / 1000;
-//   var sleepTime = 1 - iterationDuration;
-//   if (sleepTime > 0) {
-//     sleep(sleepTime);
-//   }
-// }
