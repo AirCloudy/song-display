840c05966a2c750c150a5f9f9e1db6bff59a97f2
Angelique Nehmzow <angelique@nehmzow.com>
2019-11-13 11:23:54 -0800

    server access to db

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'basicuser',
-  host: 'localhost',
+  host: '3.133.172.207',
   database: 'aircloudy',
   password: 'sdc-time',
   port: 5432,
